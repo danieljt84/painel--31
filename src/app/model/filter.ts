885bc404e85734c9fa79ed38dda236e1dc7ca18d
Daniel Moreira <danieljt84@gmail.com>
2023-02-19 13:01:19 -0300

    Alteracao no model de Filter

--- src/app/model/filter.ts
+++ src/app/model/filter.ts
@@ -1,7 +1,13 @@
-export interface Filter{
-    initialDate: string;
-    finalDate: string;
-    idBrand: number | string;
-    filter: Map<string,string[]>
+import { Chain } from "@angular/compiler";
+import { Product } from "./detail/Product";
+import { Project } from "./project";
+import { Promoter } from "./promoter";
+import { Shop } from "./shop";
 
+export interface Filter{
+    projects?:Project[],
+    shops?:Shop[],
+    promoters?:Promoter[],
+    chains?:Chain[],
+    products?:Product[]
 }
